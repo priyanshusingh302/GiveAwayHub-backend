4a196404d3403d1dd391d3bd98d17f2846611fe5
Priyanshu Singh <priyanshusingh302@gmail.com>
2023-12-05 17:15:50 +0530

    initial

---
+++ backend/src/test/java/com/store/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
